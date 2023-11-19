522590b08ca31c6f533225633675d61800bea5ec
SineRiseX <bfu01.bb@gmail.com>
2023-11-19 23:47:19 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const sqlite3 = require('better-sqlite3')
-const bcryp = require('bcrypt')
+const bcrypt = require('bcrypt')
 
 const app = express();
 const staticPath = path.join(__dirname, 'public')
@@ -50,6 +50,36 @@ app.post("/createKompani", createKompanis)
 
 app.post("/deleteKompani", deleteKompanis)
 
+app.post("/loginUser", loginUsers)
+
+async function loginUsers(request, response) {
+    const user = request.body;
+    const sqlUserType = db.prepare('SELECT userType, password, userStatus FROM users WHERE name = ?');
+    let rows = sqlUserType.all(user.name);
+
+    if (rows.length === 0) {
+        response.send({
+            ErrorMessage: `Invalid name or password`
+        });
+    } else {
+        const storedUser = rows[0];
+        if (storedUser.userStatus !== 'True') {
+            response.send({
+                ErrorMessage: `Account is not activated yet. Ask the kompani leader to activate it.`
+            });
+        } else {
+            bcrypt.compare(user.password, storedUser.password, function (err, result) {
+                if (result) {
+                    response.send({ redirectUrl: `/${storedUser.userType.toLowerCase()}-page.html` });
+                } else {
+                    response.send({
+                        ErrorMessage: `Invalid name or password`
+                    });
+                }
+            });
+        }
+    }
+}
 async function deleteKompanis(request, response) {
     const user = request.body
     removeKompanis(user.kompani)
@@ -70,7 +100,7 @@ async function deleteUsers(request){
     removeUser(user.name)
 }
 async function getUsersAdmins(request, response){
-    const sql = db.prepare("SELECT name, parents, userType, role, kompani, userStatus FROM users WHERE userType != 'admin'");
+    const sql = db.prepare("SELECT name, parents, userType, role, kompani, userStatus FROM users WHERE userType != 'Admin'");
     let rows = sql.all();
     let users = rows.map(user => ({
         name: user.name,
@@ -92,10 +122,20 @@ async function getKompanis(request, response) {
     response.send(kompanis);
 }
 
-async function createUsers(request) {
+async function createUsers(request, response) {
     const user = request.body
-    const hashPassword = bcryp.hashSync(user.password, 10)
-    insertUser(user.name, hashPassword, user.parents, user.userType, user.role, user.kompani);
+    const sql = db.prepare('SELECT name FROM users WHERE name = ?');
+    let rows = sql.all(user.name);
+    if (rows.length !== 0){
+        response.send({
+            ErrorMessage: `There is already a user with this name`
+        });
+    } else {
+        const hashPassword = bcrypt.hashSync(user.password, 10)
+        insertUser(user.name, hashPassword, user.parents, user.userType, user.role, user.kompani);
+        response.send({ redirectUrl: `/login-page.html` });
+    }
+    
 }
 
 app.listen(3000, () => {
